b240330bb28900f0648b952e30d5bc356562a4a6
Pierrick Pluchon <pierrick.pluchon@gmail.com>
2016-02-08 14:43:15 +0100

    let\'s go live

---
+++ generators/app/templates/gulpfile.js
@@ -0,0 +1,93 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var minimist = require('minimist');
+var webpack = require('gulp-webpack');
+<% if(includeImagemin) {%>
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+<% } %>
+
+var knownOptions = {
+  boolean: 'production',
+  default: { production : false }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+
+gulp.task('sass', function(){
+  if(options.production)
+  {
+    return gulp.src('src/scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass.sync({
+      outputStyle: 'compressed',
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+    .pipe(gulp.dest("public/dist/css"))
+    .pipe(browserSync.stream())
+  } else {
+    return gulp.src('src/scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass.sync({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest("public/dist/css"))
+    .pipe(browserSync.stream())
+  }
+  
+});
+
+gulp.task('webpack', function(){
+  if(options.production)
+  {
+    return gulp.src('src/js/main.js')
+    .pipe(webpack( require('./webpack.production.config.js') ))
+    .pipe(gulp.dest('public/dist/js/'))
+    .pipe(browserSync.stream());
+  } else {
+    return gulp.src('src/js/main.js')
+    .pipe(webpack(require('./webpack.config.js') ))
+    .pipe(gulp.dest('public/dist/js/'))
+    .pipe(browserSync.stream());
+  }
+})
+<% if(includeImagemin) {%>
+gulp.task('imagemin', () => {
+    return gulp.src('src/assets/**/*')
+        .pipe(plumber())
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('public/assets'));
+});
+<% } %>
+gulp.task('default', [<% if(includeImagemin) {%>'imagemin',<% } %> 'webpack', 'sass'], function(){
+  <% if(hasVhost) {%>
+      browserSync.init({
+        proxy: "<%= vhostName %>"
+      });
+  <% } else { %>
+      browserSync.init({
+        server: {
+            baseDir: "./public"
+        }
+      });
+  <% } %>
+
+  gulp.watch("src/js/**.js", ['webpack']);
+  gulp.watch("src/scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
+  gulp.watch("public/index.php").on('change', browserSync.reload);
+});
---
+++ generators/app/templates/webpack.production.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+// bar babel = require('babel-loader');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: { path: '/public/dist/js', filename:'bundle.js'},
+    node: {
+      fs: "empty"
+    },
+  module: {
+    loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    // https://github.com/babel/babel-loader#options
+                    cacheDirectory: true,
+                    presets: ['es2015', 'stage-2']
+                }
+            }
+        ],
+        preLoaders : [
+            <% if(Eslint) {%>
+                {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
+            <% } %>
+        ]
+  },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: true })
+    ],
+};
